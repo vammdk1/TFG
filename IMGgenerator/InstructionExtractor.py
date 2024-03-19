d7697ffb54720cb453a1b9dacf72f8e4da998ec7
Víctor <victor.martinez.dk@opendeusto.es>
2024-03-19 18:14:28 +0100

    lectura de todos los archivos ubicados en una ruta

--- IMGgenerator/InstructionExtractor.py
+++ IMGgenerator/InstructionExtractor.py
@@ -2,40 +2,53 @@ import os
 
 
 # Directorio donde se encuentran los archivos
-directorio = 'datos'
+
+directorio = 'OpCode'
 directorio_padre = os.path.dirname(directorio)
 
 # Lista para almacenar el contenido de los archivos
 contenido_archivos = []
 diccionario = []
 
+# Obtener la ruta completa al directorio OpCode
+directorio_padre = os.path.abspath(directorio)
 
-# Iterar sobre los archivos en el directorio
-for nombre_archivo in os.listdir(os.listdir(directorio)):
-    # Comprobar si el archivo es un archivo de texto
+# Obtener la lista de subdirectorios en el directorio OpCode
+subdirectorios = [nombre for nombre in os.listdir(directorio_padre) if os.path.isdir(os.path.join(directorio_padre, nombre))]
 
-    if nombre_archivo.endswith('.txt'):
-        # Construir la ruta completa al archivo
-        ruta_archivo = os.path.join(directorio, nombre_archivo)
-        try:
-            # Leer el contenido del archivo y almacenarlo en la lista
-            with open(ruta_archivo, 'r') as archivo:
-                contenido = archivo.readline()
-                elementos = contenido.strip().split(":")
-
-                if len(elementos)> 1 :
-                    codigo = elementos[1].strip()
-                    comandos = set(codigo.split())
-                    contenido_archivos.extend(comandos)
-        except Exception as e:
-            print("Se produjo un error al leer el archivo:", e)
+# Iterar sobre los subdirectorios
+for subdirectorio in subdirectorios:
+    # Construir la ruta completa al subdirectorio
+    ruta_subdirectorio = os.path.join(directorio_padre, subdirectorio)
+    
+    # Iterar sobre los archivos en el subdirectorio
+    for nombre_archivo in os.listdir(ruta_subdirectorio):
+        # Comprobar si el archivo es un archivo de texto
+        if nombre_archivo.endswith('.txt'):
+            # Construir la ruta completa al archivo
+            ruta_archivo = os.path.join(ruta_subdirectorio, nombre_archivo)
+            try:
+                # Leer el contenido del archivo y almacenarlo en la lista
+                with open(ruta_archivo, 'r') as archivo:
+                    contenido = archivo.readline()
+                    elementos = contenido.strip().split(":")
+    
+                    if len(elementos) > 1:
+                        codigo = elementos[1].strip()
+                        comandos = set(codigo.split())
+                        contenido_archivos.extend(comandos)
+            except Exception as e:
+                print("Se produjo un error al leer el archivo:", e)
 
 # Convertir el código de ensamblador en una matriz de valores numéricos
 tempDicc = list(set(contenido_archivos))
 diccionario = {instruccion: index for index, instruccion in enumerate(tempDicc, start=1)}
 
-# Guardar el diccionario en un archivo de texto
-ruta_diccionario = os.path.join(directorio_padre, 'diccionario.txt')
+# Obtener la ruta del directorio padre de OpCode
+directorio_padre_opcode = os.path.dirname(directorio_padre)
+
+# Guardar el diccionario en un archivo de texto en el directorio padre de OpCode
+ruta_diccionario = os.path.join(directorio_padre_opcode, 'diccionario.txt')
 with open(ruta_diccionario, 'w') as archivo_salida:
     for instruccion, valor_numerico in diccionario.items():
         archivo_salida.write(f"{instruccion}: {valor_numerico}\n")
--- IMGgenerator/prueba.py
+++ IMGgenerator/prueba.py
@@ -1,11 +1,11 @@
+import os
 from matplotlib import pyplot as plt
 import numpy as np
 
-#TODO cambiar a espectograma   
 
 # Mandar archivos para revisar la lista
-nombres_archivos = ["OpCode/malware/0a65d76177f165424a529f35c2ec4a5d.txt", "OpCode/malware/0c49f4b9c1357bbf92b3a0b2430ab49f.txt",
-                     "OpCode/malware/0d2295388bd65c7ae77fa115f827bf14.txt", "OpCode/goodware/IPv6addr.txt", "OpCode/goodware/vstp.txt", "OpCode/goodware/tickle_tcp.txt"]
+#TODO leer todos los archivos de OpCode
+
 ruta_diccionario = 'diccionario.txt'
 ruta_imagenes = 'img'
 diccionario = {}
@@ -21,7 +21,7 @@ with open(ruta_diccionario, 'r') as archivo_diccionario:
     #Sprint(diccionario)
         
 # Longitud objetivo común
-longitud_objetivo = 25000 
+longitud_objetivo = 20000 
 
 # Normalizar la longitud de las señales
 def normalizar_longitud(datos, longitud_objetivo):
@@ -36,53 +36,67 @@ def normalizar_longitud(datos, longitud_objetivo):
         #datos_normalizados = np.pad(datos, (0, longitud_objetivo - len(datos)), mode='constant')
     return datos_normalizados
 
-for nombre_archivo in nombres_archivos:
-    try:
-        # Abrir el archivo en modo de lectura
-        with open(nombre_archivo, 'r') as archivo:
-            # Leer el contenido del archivo
-            contenido = archivo.readline()
-            elementos =  contenido.strip().split(":")
-            if len(elementos)> 1 :
-                codigo = elementos[1].strip()
-                
-                # Asignar a los strings en codigo sus respectivos valores del diccionario
-                codigo_con_valores = [diccionario.get(c, 0) for c in codigo.split()]
-                #print(codigo_con_valores)
+directorio_padre = os.path.abspath("OpCode")
 
-                # Normalizar la longitud de los datos
-                datos_normalizados = normalizar_longitud(np.array(codigo_con_valores, dtype=float), longitud_objetivo)
+# Obtener la lista de subdirectorios en el directorio OpCode
+subdirectorios = [nombre for nombre in os.listdir(directorio_padre) if os.path.isdir(os.path.join(directorio_padre, nombre))]
 
-                print(len(datos_normalizados))
+# Iterar sobre los subdirectorios
+for subdirectorio in subdirectorios:
+    # Construir la ruta completa al subdirectorio
+    ruta_subdirectorio = os.path.join(directorio_padre, subdirectorio)
+    # Iterar sobre los archivos en el subdirectorio
+    for nombre_archivo in os.listdir(ruta_subdirectorio):
+        ruta_archivo = os.path.join(ruta_subdirectorio, nombre_archivo)
+        try:
+            # Abrir el archivo en modo de lectura
+            with open(ruta_archivo, 'r') as archivo:
+                # Leer el contenido del archivo
+                contenido = archivo.readline()
+                elementos =  contenido.strip().split(":")
+                if len(elementos)> 1 :
+                    codigo = elementos[1].strip()
+                    
+                    # Asignar a los strings en codigo sus respectivos valores del diccionario
+                    codigo_con_valores = [diccionario.get(c, 0) for c in codigo.split()]
+                    #print(codigo_con_valores)
 
-                # Aplicar la FFT a los datos
-                fft_resultado = np.fft.fft(datos_normalizados)
+                    # Normalizar la longitud de los datos
+                    datos_normalizados = normalizar_longitud(np.array(codigo_con_valores, dtype=float), longitud_objetivo)
+                    # Aplicar la FFT a los datos
+                    fft_resultado = np.fft.fft(datos_normalizados)
 
-                frecuencia_muestreo = 1
+                    frecuencia_muestreo = 1
 
-                # Calcular el espectrograma
-                espectrograma = plt.specgram(fft_resultado, Fs=frecuencia_muestreo, cmap='viridis')
+                    # Calcular el espectrograma
+                    espectrograma = plt.specgram(fft_resultado, Fs=frecuencia_muestreo, cmap='viridis')
 
-                valor_minimo_colorbar = 0  # Valor mínimo deseado en la barra de color (dB)
-                valor_maximo_colorbar = 100   # Valor máximo deseado en la barra de color (dB)
-                espectrograma[3].set_clim(valor_minimo_colorbar, valor_maximo_colorbar)
-                # Ocultar los números en los ejes x e y
-                plt.colorbar().remove()
-                plt.xticks([])
-                plt.yticks([])
-                #eliminar el margen
-                plt.tight_layout(pad=0)
-                nombre_archivo = nombre_archivo.split("/")
-                nombre_archivo = nombre_archivo[2]
-                nombre_archivo = nombre_archivo.split(".")
-                nombre_archivo = nombre_archivo[0]
-                plt.savefig('img/'+nombre_archivo+'_imagen.png')
-                #plt.show()
-                plt.clf()
+                    valor_minimo_colorbar = 0  # Valor mínimo deseado en la barra de color (dB)
+                    valor_maximo_colorbar = 100   # Valor máximo deseado en la barra de color (dB)
+                    espectrograma[3].set_clim(valor_minimo_colorbar, valor_maximo_colorbar)
+                    # Ocultar los números en los ejes x e y
+                    plt.colorbar().remove()
+                    plt.xticks([])
+                    plt.yticks([])
+                    #eliminar el margen
+                    plt.tight_layout(pad=0)
+                    
+                    
 
-    except FileNotFoundError:
-        print("El archivo especificado no se encontró.")
-    except Exception as e:
-        print("Se produjo un error al leer el archivo:", e)
+                    # Guardar el espectrograma en un archivo de imagen
+                    ruta_archivo = ruta_archivo.split("\\")
+                    carpeta = ruta_archivo[len(ruta_archivo)-2]
+                    nombre_archivo = ruta_archivo[len(ruta_archivo)-1].split(".")[0]
 
+                    if not os.path.exists('img'):
+                        os.makedirs('img')
+                    if not os.path.exists('img/'+carpeta):
+                        os.makedirs('img/'+carpeta)
 
+                    plt.savefig('img/'+carpeta+'/'+nombre_archivo+'_imagen.png')
+                    #plt.show()
+                    plt.clf()
+        except FileNotFoundError:
+            print("El archivo especificado no se encontró.")
+        except Exception as e:
+            print("Se produjo un error al leer el archivo:", e)
