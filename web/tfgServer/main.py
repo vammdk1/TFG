7d9f740a360f6c41eae19b5010328eafac98adec
Víctor <victor.martinez.dk@opendeusto.es>
2024-06-19 09:28:39 +0200

    debug false

--- web/tfgServer/main.py
+++ web/tfgServer/main.py
@@ -123,5 +123,5 @@ def get_predictions():
     return jsonify(resultados)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
